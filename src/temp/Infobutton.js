55cd0819aee38d5c786696c317598dd9b028eb3a
Fagyi <vinczef.o@gmail.com>
2020-02-26 22:03:26 +0100

    asddd

---
+++ src/temp/Infobutton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+import './Infobutton.css';
+import './App.css';
+
+function Infobutton() {
+  return (
+  
+    <div class="container">
+    <div class="inner">
+        <span>i</span>
+        <h1>Hey</h1>
+        <p>This is an informative card that will tell you something that's... well, important!</p>
+    </div>
+</div>
+         
+  );
+};
+
+
+export default Infobutton;
